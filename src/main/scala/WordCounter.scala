c4199151e969c7369c6c9316c0165288ffc70b24
aditya <adityabhatkar11@gmail.com>
2018-02-11 01:49:34 -0500

    improved word counter to clean words and sort by count

--- src/main/scala/WordCounter.scala
+++ src/main/scala/WordCounter.scala
@@ -3,10 +3,16 @@ object WordCounter {
   def main(args: Array[String]): Unit = {
     val sc = BoilerPlateUtil.prepareSparkContext(args(1), args(2))
     val book = BoilerPlateUtil.readInputFile(args(0), sc)
-    val wordCount = book.flatMap(line => line.split(" ")).countByValue()
-    for((word, count) <- wordCount) {
+    val words = book.flatMap(line => normalizeWord(line))
+    val wordCount = words.map(word => (word, 1)).reduceByKey((a , b) => a + b)
+    val wordCountSorted = wordCount.map(wc => wc.swap).sortByKey().collect()
+    for((count, word) <- wordCountSorted) {
       val cleanWord = word.replaceAll("[^\\x20-\\x7e]", "")
       println(s"${cleanWord} ${count}")
     }
+  }
+
+  def normalizeWord(line: String): Array[String] = {
+    return line.split("\\W+").map(word => word.toLowerCase())
   }
 }
